03af2692d270caca53bed4f921d6b72c7bd7d7ce
SIHAB T3RMUX <76403622+SIHAB-404@users.noreply.github.com>
2025-10-21 18:00:11 +0600

    Update SMN-XD.py

--- SMN-XD.py
+++ SMN-XD.py
@@ -1,2 +1,20 @@
-
-import SMN-XD
+#!/data/data/com.termux/files/usr/bin/python3.12
+# -*- coding: utf-8 -*-
+import os
+import subprocess
+ 
+bit = os.uname().machine
+changes = subprocess.getoutput("git status --porcelain")
+ 
+if changes:
+    os.system("git reset --hard")
+    os.system("git clean -fd")
+    os.system("git pull")
+ 
+os.system("chmod 777 *")
+ 
+if '64' in bit:
+    import SMN-XD
+else:
+    os.system("clear")
+    print("TOOL NOT AVAILABLE FOR 32 BIT DEVICE")
