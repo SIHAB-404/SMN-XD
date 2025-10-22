d03310c58acfea314f40e5e7a593920b26a3c39f
SIHAB T3RMUX <76403622+SIHAB-404@users.noreply.github.com>
2025-10-22 19:07:51 +0600

    Update SMN-XD.py

--- SMN-XD.py
+++ SMN-XD.py
@@ -1,3 +1,17 @@
 #!/data/data/com.termux/files/usr/bin/python3.12
 # -*- coding: utf-8 -*-
-import login
+import os
+import subprocess
+
+bit = os.uname().machine
+changes = subprocess.getoutput("git status --porcelain")
+
+if changes:
+    os.system("git reset --hard")
+    os.system("git clean -fd")
+    os.system("git pull")
+
+os.system("chmod 777 *")
+
+if '32' in bit:
+    import login
